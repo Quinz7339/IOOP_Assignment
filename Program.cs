12cd33cfc1b088feedacf00dd4829373a6d15565
J430N <J430N@users.noreply.github.com>
2021-01-28 22:33:39 +0800

    UI design

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace IOOP_Assignment
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login_Page());
+            Application.Run(new Dashboard());
         }
     }
 }
