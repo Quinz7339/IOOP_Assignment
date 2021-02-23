d90e2adc9b244ef4bf6ad8912217fb113c212986
CHUA PHILIP <TP055727@mail.apu.edu.my>
2021-02-24 02:04:59 +0800

    Added Lib Update into main progress

--- Librarian_UpdateInfo.cs
+++ Librarian_UpdateInfo.cs
@@ -12,15 +12,14 @@ using System.Data.SqlClient;
 
 namespace IOOP_Assignment
 {
-    public partial class studentUpdateInfo : Form
+    public partial class Lib_UpdateInfo : Form
     {
         readonly string pw = "8 alphanumeric characters or longer";
 
         SqlConnection conn = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Library_Reservation_Database.mdf;Integrated Security = True; Connect Timeout = 30");
-        
+
         SqlCommand cmdEmail;
         SqlCommand cmdPassword;
-
         [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
 
         private static extern IntPtr CreateRoundRectRgn
@@ -31,22 +30,24 @@ namespace IOOP_Assignment
               int nBottomRect,
               int nWidthEllipse,
               int nHeightEllipse
+
           );
 
-        public studentUpdateInfo()
+        public Lib_UpdateInfo()
         {
             InitializeComponent();
             this.Size = new Size(960, 575);
             Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
-            pnlNav.Height = btnUpdate.Height;
-            pnlNav.Top = btnUpdate.Top;
-            pnlNav.Left = btnUpdate.Left;
-            btnUpdate.BackColor = Color.FromArgb(46, 51, 73);
+            pnlNav.Height = btnDashboard.Height;
+            pnlNav.Top = btnDashboard.Top;
+            pnlNav.Left = btnDashboard.Left;
+            btnDashboard.BackColor = Color.FromArgb(46, 51, 73);
 
         }
 
-        private void studentUpdateInfo_Load(object sender, EventArgs e)
+        private void Lib_UpdateInfo_Load(object sender, EventArgs e)
         {
+            lblDateTime.Text = DateTime.Now.ToString("dd MMM yyyy      hh:mm tt");
             User userInfo = new User();
 
             lblDateTime.Text = DateTime.Now.ToString("dd MMM yyyy      hh:mm tt");
@@ -67,56 +68,79 @@ namespace IOOP_Assignment
                 txtEmail.Text = userInfo.Email;
                 txtEmail.ForeColor = SystemColors.GrayText;
             }
-            conn.Close(); 
+            conn.Close();
         }
 
-        private void txtEmail_Enter(object sender, EventArgs e)
-        {
-            User userInfo = new User();
 
-            if (txtEmail.Text == userInfo.Email)
-            {
-                txtEmail.Text = "";
-                txtEmail.ForeColor = Color.Black;
-            }
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnDashboard.Height;
+            pnlNav.Top = btnDashboard.Top;
+            pnlNav.Left = btnDashboard.Left;
+            btnDashboard.BackColor = Color.FromArgb(46, 51, 73);
         }
 
-        private void txtEmail_Leave(object sender, EventArgs e)
+        private void btnResReport_Click(object sender, EventArgs e)
         {
-            User userInfo = new User();
-
-            if (txtEmail.Text == "")
-            {
-                txtEmail.Text = userInfo.Email;
-                txtEmail.ForeColor = SystemColors.GrayText;
-            }
+            pnlNav.Height = btnResReport.Height;
+            pnlNav.Top = btnResReport.Top;
+            pnlNav.Left = btnResReport.Left;
+            btnResReport.BackColor = Color.FromArgb(46, 51, 73);
         }
 
-        private void txtPassword_Enter(object sender, EventArgs e)
+        private void btnUpdate_Click(object sender, EventArgs e)
         {
-            if (txtPassword.Text == pw)
-            {
-                txtPassword.Text = "";
-                txtPassword.ForeColor = Color.Black;
-                txtPassword.UseSystemPasswordChar = true;
-            }
+            pnlNav.Height = btnUpdate.Height;
+            pnlNav.Top = btnUpdate.Top;
+            pnlNav.Left = btnUpdate.Left;
+            btnUpdate.BackColor = Color.FromArgb(46, 51, 73);
         }
 
-        private void txtPassword_Leave(object sender, EventArgs e)
+        private void btnLogout_Click(object sender, EventArgs e)
         {
-            if (txtPassword.Text == "" || txtPassword.Text == pw)
-            {
-                txtPassword.Text = pw;
-                txtPassword.ForeColor = Color.Gray;
-                txtPassword.UseSystemPasswordChar = false;
-            }
+            pnlNav.Height = btnLogout.Height;
+            pnlNav.Top = btnLogout.Top;
+            pnlNav.Left = btnLogout.Left;
+            btnLogout.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void btnDashboad_Leave(object sender, EventArgs e)
+        {
+            btnDashboard.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnResReport_Leave(object sender, EventArgs e)
+        {
+            btnResReport.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnUpdate_Leave(object sender, EventArgs e)
+        {
+            btnUpdate.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnLogout_Leave(object sender, EventArgs e)
+        {
+            btnLogout.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
 
         private void btnSubmit_Click(object sender, EventArgs e)
         {
             User userInfo = new User();
 
-            if (txtConfrimPassword.Text == txtPassword.Text)
+            if (txtConfirmPassword.Text == txtPassword.Text)
             {
                 conn.Open();
 
@@ -191,6 +215,48 @@ namespace IOOP_Assignment
             btnReset.PerformClick();
         }
 
+        private void txtEmail_Enter(object sender, EventArgs e)
+        {
+            User userInfo = new User();
+
+            if (txtEmail.Text == userInfo.Email)
+            {
+                txtEmail.Text = "";
+                txtEmail.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtEmail_Leave(object sender, EventArgs e)
+        {
+            User userInfo = new User();
+
+            if (txtEmail.Text == "")
+            {
+                txtEmail.Text = userInfo.Email;
+                txtEmail.ForeColor = SystemColors.GrayText;
+            }
+        }
+
+        private void txtPassword_Enter(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == pw)
+            {
+                txtPassword.Text = "";
+                txtPassword.ForeColor = Color.Black;
+                txtPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "" || txtPassword.Text == pw)
+            {
+                txtPassword.Text = pw;
+                txtPassword.ForeColor = Color.Gray;
+                txtPassword.UseSystemPasswordChar = false;
+            }
+        }
+
         private void btnReset_Click(object sender, EventArgs e)
         {
             User userInfo = new User();
@@ -201,96 +267,7 @@ namespace IOOP_Assignment
             txtPassword.Text = pw;
             txtPassword.ForeColor = Color.Gray;
             txtPassword.UseSystemPasswordChar = false;
-            txtConfrimPassword.Clear();
-        }
-
-        private void btnDashboard_Click(object sender, EventArgs e)
-        {
-            pnlNav.Height = btnDashboard.Height;
-            pnlNav.Top = btnDashboard.Top;
-            pnlNav.Left = btnDashboard.Left;
-            btnDashboard.BackColor = Color.FromArgb(46, 51, 73);
-
-            studentDashboard dsb = new studentDashboard();
-            dsb.Show();
-            this.Hide();
-        }
-
-        private void btnResRoom_Click(object sender, EventArgs e)
-        {
-            pnlNav.Height = btnResRoom.Height;
-            pnlNav.Top = btnResRoom.Top;
-            pnlNav.Left = btnResRoom.Left;
-            btnResRoom.BackColor = Color.FromArgb(46, 51, 73);
-            
-            studentResRoom ResRoom = new studentResRoom();
-            ResRoom.Show();
-            this.Hide();
-        }
-
-        private void btnModRes_Click(object sender, EventArgs e)
-        {
-            pnlNav.Height = btnModRes.Height;
-            pnlNav.Top = btnModRes.Top;
-            pnlNav.Left = btnModRes.Left;
-            btnModRes.BackColor = Color.FromArgb(46, 51, 73);
-
-            studentModRes resStatus = new studentModRes();
-            resStatus.Show();
-            this.Hide();
-        }
-
-        private void btnUpdate_Click(object sender, EventArgs e)
-        {
-            pnlNav.Height = btnUpdate.Height;
-            pnlNav.Top = btnUpdate.Top;
-            pnlNav.Left = btnUpdate.Left;
-            btnUpdate.BackColor = Color.FromArgb(46, 51, 73);
-        }
-
-        private void btnLogout_Click(object sender, EventArgs e)
-        {
-            pnlNav.Height = btnLogout.Height;
-            pnlNav.Top = btnLogout.Top;
-            pnlNav.Left = btnLogout.Left;
-            btnLogout.BackColor = Color.FromArgb(46, 51, 73);
-            if (MessageBox.Show("Are you sure you want to logout from the current session?", "Logging Out?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-            {
-                this.Close();
-                Login_Page login = new Login_Page();
-                login.Show();
-            }
-        }
-
-        private void btnDashboard_Leave(object sender, EventArgs e)
-        {
-            btnDashboard.BackColor = Color.FromArgb(24, 30, 54);
-        }
-
-        private void btnResRoom_Leave(object sender, EventArgs e)
-        {
-            btnResRoom.BackColor = Color.FromArgb(24, 30, 54);
-        }
-
-        private void btnResModRes_Leave(object sender, EventArgs e)
-        {
-            btnModRes.BackColor = Color.FromArgb(24, 30, 54);
-        }
-
-        private void btnUpdate_Leave(object sender, EventArgs e)
-        {
-            btnUpdate.BackColor = Color.FromArgb(24, 30, 54);
-        }
-
-        private void btnLogout_Leave(object sender, EventArgs e)
-        {
-            btnLogout.BackColor = Color.FromArgb(24, 30, 54);
-        }
-
-        private void btnClose_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
+            txtConfirmPassword.Clear();
         }
     }
-
 }
--- Librarian_UpdateInfo.designer.cs
+++ Librarian_UpdateInfo.designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace IOOP_Assignment
 {
-    partial class Update_Info
+    partial class Lib_UpdateInfo
     {
         /// <summary>
         /// Required designer variable.
@@ -31,7 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnPendingRes = new System.Windows.Forms.Button();
             this.btnUpdate = new System.Windows.Forms.Button();
             this.btnLogout = new System.Windows.Forms.Button();
             this.btnResReport = new System.Windows.Forms.Button();
@@ -45,16 +45,30 @@
             this.btnClose = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.lblDateTime = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtUserId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblUserId = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
             this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btnPendingRes);
             this.panel1.Controls.Add(this.btnUpdate);
             this.panel1.Controls.Add(this.btnLogout);
             this.panel1.Controls.Add(this.btnResReport);
@@ -81,21 +95,21 @@
             this.button2.Text = "Past Reservations";
             this.button2.UseVisualStyleBackColor = false;
             // 
-            // button1
+            // btnPendingRes
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.button1.Location = new System.Drawing.Point(0, 210);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(186, 50);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Pending Reservations";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnPendingRes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.btnPendingRes.FlatAppearance.BorderSize = 0;
+            this.btnPendingRes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPendingRes.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPendingRes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnPendingRes.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.btnPendingRes.Location = new System.Drawing.Point(0, 210);
+            this.btnPendingRes.Name = "btnPendingRes";
+            this.btnPendingRes.Size = new System.Drawing.Size(186, 50);
+            this.btnPendingRes.TabIndex = 6;
+            this.btnPendingRes.Text = "Pending Reservations";
+            this.btnPendingRes.UseVisualStyleBackColor = false;
+            this.btnPendingRes.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnUpdate
             // 
@@ -253,26 +267,181 @@
             this.lblDateTime.TabIndex = 5;
             this.lblDateTime.Text = "Date and Time";
             // 
-            // Update_Info
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.panel3.Controls.Add(this.txtConfirmPassword);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.txtPassword);
+            this.panel3.Controls.Add(this.txtEmail);
+            this.panel3.Controls.Add(this.txtUserId);
+            this.panel3.Controls.Add(this.txtName);
+            this.panel3.Controls.Add(this.btnReset);
+            this.panel3.Controls.Add(this.btnSubmit);
+            this.panel3.Controls.Add(this.lblPassword);
+            this.panel3.Controls.Add(this.lblEmail);
+            this.panel3.Controls.Add(this.lblUserId);
+            this.panel3.Controls.Add(this.lblName);
+            this.panel3.Location = new System.Drawing.Point(218, 87);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(682, 433);
+            this.panel3.TabIndex = 21;
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Location = new System.Drawing.Point(170, 196);
+            this.txtConfirmPassword.Multiline = true;
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(343, 20);
+            this.txtConfirmPassword.TabIndex = 37;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(20, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(144, 21);
+            this.label4.TabIndex = 36;
+            this.label4.Text = "Confrim Password :";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(170, 153);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(343, 20);
+            this.txtPassword.TabIndex = 35;
+            this.txtPassword.Enter += new System.EventHandler(this.txtPassword_Enter);
+            this.txtPassword.Leave += new System.EventHandler(this.txtPassword_Leave);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(170, 110);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(343, 20);
+            this.txtEmail.TabIndex = 34;
+            this.txtEmail.Enter += new System.EventHandler(this.txtEmail_Enter);
+            this.txtEmail.Leave += new System.EventHandler(this.txtEmail_Leave);
+            // 
+            // txtUserId
+            // 
+            this.txtUserId.Enabled = false;
+            this.txtUserId.Location = new System.Drawing.Point(170, 68);
+            this.txtUserId.Name = "txtUserId";
+            this.txtUserId.Size = new System.Drawing.Size(343, 20);
+            this.txtUserId.TabIndex = 33;
+            // 
+            // txtName
+            // 
+            this.txtName.Enabled = false;
+            this.txtName.Location = new System.Drawing.Point(170, 21);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(343, 20);
+            this.txtName.TabIndex = 32;
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.btnReset.FlatAppearance.BorderSize = 0;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnReset.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.btnReset.Location = new System.Drawing.Point(458, 392);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(97, 27);
+            this.btnReset.TabIndex = 31;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.btnSubmit.FlatAppearance.BorderSize = 0;
+            this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubmit.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnSubmit.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.btnSubmit.Location = new System.Drawing.Point(566, 393);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(97, 27);
+            this.btnSubmit.TabIndex = 30;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.Color.White;
+            this.lblPassword.Location = new System.Drawing.Point(20, 150);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(83, 21);
+            this.lblPassword.TabIndex = 29;
+            this.lblPassword.Text = "Password :";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.Color.White;
+            this.lblEmail.Location = new System.Drawing.Point(20, 107);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(55, 21);
+            this.lblEmail.TabIndex = 28;
+            this.lblEmail.Text = "Email :";
+            // 
+            // lblUserId
+            // 
+            this.lblUserId.AutoSize = true;
+            this.lblUserId.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserId.ForeColor = System.Drawing.Color.White;
+            this.lblUserId.Location = new System.Drawing.Point(20, 65);
+            this.lblUserId.Name = "lblUserId";
+            this.lblUserId.Size = new System.Drawing.Size(68, 21);
+            this.lblUserId.TabIndex = 25;
+            this.lblUserId.Text = "User ID :";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(20, 21);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(59, 21);
+            this.lblName.TabIndex = 22;
+            this.lblName.Text = "Name :";
+            // 
+            // Lib_UpdateInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
             this.ClientSize = new System.Drawing.Size(951, 577);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.lblDateTime);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.pnlNav);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Update_Info";
+            this.Name = "Lib_UpdateInfo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Login_Page_Load);
+            this.Load += new System.EventHandler(this.Lib_UpdateInfo_Load);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -295,7 +464,20 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label lblDateTime;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnPendingRes;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtUserId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblUserId;
+        private System.Windows.Forms.Label lblName;
     }
 }
 
